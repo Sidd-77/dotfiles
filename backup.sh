1ee8717fd9997deba22239f1de288bc3db6aa6ea
Sidd-77 <siddharthsalunkhe2003@gmail.com>
2024-09-16 13:16:04 +0530

    backup Mon Sep 16 01:16:04 PM IST 2024

---
+++ backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+echo "[LOG] Taking backup of dotfiles"
+
+echo "[LOG] Coping files"
+
+cp /etc/anacrontab ~/dotfiles/
+cp -r ~/.config/kitty ~/dotfiles/ 
+cp -r ~/.config/i3 ~/dotfiles/ 
+cp -r ~/.config/nvim ~/dotfiles/ 
+cp -r ~/.config/polybar ~/dotfiles/ 
+cp -r ~/.config/xfce4 ~/dotfiles/ 
+cp -r ~/.config/rofi ~/rofi/
+cp ~/.zshrc ~/dotfiles/zsh/
+
+echo "[LOG] Coping done"
+
+cd ~/dotfiles/ 
+
+echo "[LOG] Commiting changes"
+git add .
+git commit -m "backup $(date)"
+echo "[LOG] Changes commited"
+
+echo "[LOG] Pushing changes to remote repository"
+git push
+echo "[LOG] Done"
+
+
+echo "[LOG] Backup done"
+
+
